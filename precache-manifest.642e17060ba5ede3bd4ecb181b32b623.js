9f9f171413dbdd4edb750bbb197af9368002d58b
Bishoy Khalil <bftawfik@gmail.com>
2024-06-03 10:45:13 +0300

    add new build

---
+++ precache-manifest.642e17060ba5ede3bd4ecb181b32b623.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddb426fb0c5f63f91fbe",
+    "url": "/static/css/main.f974dfd5.chunk.css"
+  },
+  {
+    "revision": "ddb426fb0c5f63f91fbe",
+    "url": "/static/js/main.ddb426fb.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9fa6cb5560fcc5d6ec9b",
+    "url": "/static/js/2.9fa6cb55.chunk.js"
+  },
+  {
+    "revision": "d974dd60686f1f437262ef155a040423",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c5d33f21ea2567e7af38e22c705c81e.js"
+  "/precache-manifest.642e17060ba5ede3bd4ecb181b32b623.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
